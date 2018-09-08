b5b949e75bcc97bdfb875379390b68270e7579fc
Ryan Szrama <ryan.szrama@gmail.com>
2018-09-07 23:07:33 -0400

    Add comments to the build script with a link to Browserify.

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+# Delete the previous build and recreate the build folders.
 rm -rf build
 mkdir build
 mkdir build/lib
+
+# Copy the libraries and HTML file to the build.
 cp -R static-lib/* build/lib
 cp src/html/* build
+
+# Use Browserify to compile the codebase into a single JavaScript file in the
+# build directory, maintaining support for require() in the browser.
+# @see http://browserify.org/
 cd src/js
-browserify Game.js -o ../../build/rl-min.js 
+browserify Game.js -o ../../build/rl-min.js
